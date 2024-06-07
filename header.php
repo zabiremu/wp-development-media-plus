8703dab4c154c17a135b16d598cdf5301ca21bc1
zabir emu <zabirraihan570@gmail.com>
2024-06-07 10:12:54 +0600

    show category completed

--- header.php
+++ header.php
@@ -90,7 +90,7 @@
                         <div class="col-lg-3 col-md-3 col-12">
                             <!-- Start Logo -->
                             <div class="logo">
-                                <a href="index.html"><img src="<?php echo get_template_directory_uri() ?>/assets/image/logo.png" alt="#"></a>
+                                <a href="<?php echo get_site_url ()?>"><img src="<?php echo get_template_directory_uri() ?>/assets/image/logo.png" alt="#"></a>
                             </div>
                             <!-- End Logo -->
                             <!-- Mobile Nav -->
